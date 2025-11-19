1f5f2afb7921cf6cc396184508501fe60f8b7711
alumnotics-eng <alumno.tics@cbtis118.edu.mx>
2025-11-19 18:15:44 +0000

    Se agrega practica 2 y resultados

---
+++ practica2.mongodb.js
@@ -0,0 +1,26 @@
+use ("BaseDatosPrueba");
+db.comida.insertMany([{
+    
+    Nombre: "pizza",
+    Calorias: 1234
+},
+{
+    Nombre: "Hamburguesa",
+    Calorias: 875
+},
+{
+    Nombre: "Sushi",
+    Calorias: 692
+
+},
+{
+    Nombre: "Burrito",
+    Calorias: 445
+
+},
+{
+    Nombre: "Tacos",
+    Calorias: 1120
+
+}
+])
